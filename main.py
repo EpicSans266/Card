fc839fabc9fc025e7fd67cef21fc9b980e8c801b
EpicSans266 <135729474+EpicSans266@users.noreply.github.com>
2025-04-17 15:25:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
 
-# Событие при готовности бота
 @bot.event
 async def on_ready():
     print(f"Бот {bot.user} успешно запущен!")
 
-# Команда для вывода визитки
 @bot.command()
 async def визитка(ctx):
     визитка_текст = """
